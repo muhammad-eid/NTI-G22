8eb56a99c080880a34ca2804a2c862303e17bc90
marwaMRadwan <marwaradwan872@hotmail.com>
2022-09-05 11:29:28 +0200

    s2

--- session2/script.js
+++ session2/script.js
@@ -1,13 +1,22 @@
 const taskHeads =  ["title", "content", "age"]
 // const tasks = []
 const addForm = document.querySelector("#addForm")
+const dataWrap = document.querySelector("#dataWrap")
 
-const readFromStorage = () => {
-
+const readFromStorage = (key= "tasks") => {
+    let data
+    try{
+        data = JSON.parse(localStorage.getItem(key)) || []
+        if(!Array.isArray(data)) throw new Error("data is not an array")
+    }
+    catch(e){
+        data = []
+    }
+    return data
 }
 
-const writeToStorage = (data) => {
-
+const writeToStorage = (data, key="tasks") => {
+    localStorage.setItem(key, JSON.stringify(data))
 }
 
 const createTaskObject = (addForm) =>{
@@ -15,11 +24,35 @@ const createTaskObject = (addForm) =>{
     taskHeads.forEach(head => task[head]= addForm.elements[head].value)
     return task 
 }
+if(addForm){
+    addForm.addEventListener("submit", function(e){
+        e.preventDefault()
+        const task =createTaskObject(this)
+        const tasks = readFromStorage()
+        tasks.push(task)
+        writeToStorage(tasks)
+        window.location.href="index.html"
+    })
+}
+
+if(dataWrap){
+    const tasks = readFromStorage()
+    tasks.forEach(task=>{
+        let tr = document.createElement("tr")
+        dataWrap.appendChild(tr)
+        let td = document.createElement("td")
+        td.textContent = task.id
+        tr.appendChild(td)
+        td = document.createElement("td")
+        td.textContent = task.title
+        tr.appendChild(td)
+        td = document.createElement("td")
+        td.textContent = task.content
+        tr.appendChild(td)
+        td = document.createElement("td")
+        td.textContent = task.age
+        tr.appendChild(td)
 
 
-addForm.addEventListener("submit", function(e){
-    e.preventDefault()
-    createTaskObject(this)
-    const tasks = localStorage.getItem("tasks")
-    tasks.push(task)
-})
+    })
+}
